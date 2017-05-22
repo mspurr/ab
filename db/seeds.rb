b4d775d4950ec7a35537023286e3ab7c88b2613b
Mathias Spurr <scitus@hotmail.dk>
2017-05-22 03:07:50 +0200

    Added devise, not finished setting it up though. Did some small design work as well.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Document.delete_all
-for i in 1..4 do
+for i in 1..16 do
 	Document.create(title: "Test kontrakt #{i}",
 	description: 'Her skal vaere en beskrivelse af dokumentet. Det kan vaere en kort beskrivelse af kontraktens formaal.',
 	name: 'Peter Hansen')
